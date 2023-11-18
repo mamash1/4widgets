bef46c374ad72944a637559292671c67eaf0bd5d
mamas vartholomeou <59097382+mamash1@users.noreply.github.com>
2023-11-18 16:23:33 +0200

    Add files via upload

---
+++ weather_web_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import requests, json
+
+api_key = "xxxx-xxxx-xxxx-xxxx" # Enter your API key here
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+
+def get_weather_info(city_name):
+    """
+    A function to generate retrieve weather data
+
+    :return:
+    """
+    url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(url)
+    x = response.json()
+    temperature = '----'
+    pressure = '----'
+    humidity = '----'
+    weather_condition = '----'
+    if x["cod"] != "404":
+        y = x["main"]
+        temperature = y["temp"]
+        pressure = y["pressure"]
+        humidity = y["humidity"]
+        weather_condition = x["weather"][0]["description"]
+    else:
+        print(" City Not Found ")
+
+
+    return temperature, humidity, weather_condition
+
+st.title("Random London Weather App") # Create a Streamlit web app
+
+
+temperature, humidity, weather_condition = get_weather_info('Limassol')
+st.write(f"Temperature: {temperature}°C")
+st.write(f"Humidity: {humidity}%")
+st.write(f"Condition: {weather_condition}")
+
+
